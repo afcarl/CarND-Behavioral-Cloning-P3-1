aed8e2ef05492858344e5a7ca0a311cb498fdffc
Manfred Diaz <takeitallsource@gmail.com>
2017-03-01 14:55:31 -0500

    [FIXED] Resnet50 Transfer Learning Pipeline.

--- simplenn.py
+++ simplenn.py
@@ -1,10 +1,8 @@
-import datahandler as dh
+import datahandler
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Cropping2D, Lambda
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-
-X_train, y_train = dh.get()
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3), output_shape=(160,320,3)))
@@ -19,8 +17,13 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 # model.load_weights('model.h5')
+train_samples, validation_samples = datahandler.split_data()
+
+train_generator = datahandler.generator(train_samples)
+validation_generator = datahandler.generator(validation_samples)
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, verbose=1, nb_epoch=5)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples) * 4,
+                    validation_data=validation_generator, nb_epoch=1, nb_val_samples=len(validation_samples) * 4)
 model.save('model.h5')
 
-exit()
